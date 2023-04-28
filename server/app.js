1c9071ce216997e068d01784d02b0db9e7e2c8db
joshjk <jokrienke@comcast.net>
2023-04-27 21:31:58 -0700

    completed

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-// Your code here
+
+app.use( "/stylesheets" , express.static("assets/css"))
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
